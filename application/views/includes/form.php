a35c884739aae4e6c6d647fb468ad1b24759be69
hotheadhacker <isalmanqureshi@gmail.com>
2021-11-08 14:12:51 +0530

    upate and close rest working on production last commit

--- application/views/includes/form.php
+++ application/views/includes/form.php
@@ -3,7 +3,7 @@
         <h5 class="text-info m-4">Enter a URL to Shrink</h5>
         <form method="post" class="m-4">
         <?php echo validation_errors(); ?> 
-        <input type="text" name="urlField" value="" placeholder="https://example.com/some-long-string" autocomplete="off"  />
+        <input type="url" name="urlField" value="" placeholder="https://example.com/some-long-string" autocomplete="off" required />
         <input type="Submit" value="submit">
         </form>
     </center>
